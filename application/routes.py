f613dd9d4c1ce0724f1cbe94e29073ab728a4c0e
Olivia Xie <olivia.xie@hotmail.com>
2021-01-03 18:53:38 -0500

    first commit

---
+++ application/routes.py
@@ -0,0 +1,20 @@
+import os
+from application import app
+from flask import render_template, request
+from flask_dropzone import Dropzone
+
+# Dropzone settings
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image'
+app.config['DROPZONE_MAX_FILES'] = 1
+app.config['DROPZONE_UPLOAD_ON_CLICK'] = True
+
+dropzone = Dropzone(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
+    return render_template('index.html')
